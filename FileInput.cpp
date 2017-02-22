7da907cb4d9501a24ef6f334abb9b3ab9a579b6b
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2017-02-21 22:28:25 -0500

    Final Dump of FileInput
    Was employeePay.cpp, now FileInput.cpp.
    Input and output are here too.

--- FileInput.cpp
+++ FileInput.cpp
@@ -1,13 +1,18 @@
 /*****
-My file - make header
+Name: Isaac Bennett
+Course: CIT-163-202
+Date: Febuary 21, 2017
+******
+Files for Assignment: employeePay.cpp
+Purpose of file: File input program that calculates new employee pay. Also outputs to other file.
 ****/
 
 
-#include <iostream>
+#include<iomanip>
 #include<fstream>
 #include<string>
-//#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -22,103 +27,68 @@ int main()
 	project4Out.clear();
 	project4Out.close();
 
-	//Setup paths to output
-	/*project4Out.open("example.txt");
-	project4Out << ("Hello World");
-	project4Out << ("This is a test");
-	project4Out.close();*/
-
-	//Input file
-	// Does not work for some reason fin.open("D:\Cartwright C++\FileInput\FileInput\Project4_Pay.txt");
-
-	//Works
-	//fin.open("Project4_Pay.txt");
-
-	// OR
+	//Sets string for user input
 	string fileName;
-	//string fileTitle;
+	string fileTitle;
+
+	//Ask for user input
 	cout << "Enter the path to your input file, include file in path:";
-	//cin >> fileName >> fileTitle;
+	//Gets user input
 	getline(cin, fileName);
-	fin.open(fileName.c_str());
-	/*cout << "Please give the file name:";
-	getline(cin, fileTitle);
-	cout << fileName << " || " << fileTitle << endl;
-	string fileLocation = fileName + fileTitle;
-	fin.open(fileLocation);*/
-	//cout << "Enter file name:";
-	//cin >> fileTitle;
-	//fin.open(fileName);
-	//Project4_Pay.txt
+	//Had an error where fin.open would overload, put actual fileName in seperate string
+	//Takes last section of fileName to find file name
+	fileTitle = fileName.c_str();
 
+	//Opens file
+	fin.open(fileTitle);
+
+	//Setup variables to pull from file
 	string FirstName;
-
 	string LastName;
-
 	double currentSalary = 0.0;
-
 	double percentIncrease = 0.0;
-
 	double updatedSalary = 0.0;
 
+	//Set precision
 	cout << setprecision(2);
 
+	//Begin while loop
+	//While loop pulls data from file and displays to console, also outputs to an output file
+	while (!fin.eof()) {
 
+		//Pulls variables from file
+		fin >> LastName;
+		fin >> FirstName;
+		fin >> currentSalary;
+		fin >> percentIncrease;
 
-	// use a EOF While loop to loop through an input file
-	// to be safe, always PRIME your EOF While loops
+		//Displays to console pulled data
+		cout << "Last Name:" << LastName << endl;
+		cout << "First Name: " << FirstName << endl;
+		cout << "Current Salary: " << fixed << currentSalary << endl;
+		cout << "Percent Increase: " << fixed << percentIncrease << endl;
 
-	//fin >> input;           // reads first eligible item (in this case, an integer) from the file
-							//check for string or int, if string ignore, if int begin calc
-	//fin >> LastName1;	//use fin.eof for while loop //fin.eof()
+		//Calculates updated salary
+		percentIncrease = percentIncrease / 100.0;
+		updatedSalary = (currentSalary) * (1 + percentIncrease);
 
-	//NOTE: adding a space at the end of the file causes
-		while (!fin.eof()) {
-			fin >> LastName;
-			fin >> FirstName;
-			fin >> currentSalary;
-			fin >> percentIncrease;
-			cout << "Last Name:" << LastName << endl;
-			cout << "First Name: " << FirstName << endl;
-			cout << "Current Salary: " << fixed << currentSalary << endl;
-			cout << "Percent Increase: " << fixed << percentIncrease << endl;
-			//Do updated salary
-			percentIncrease = percentIncrease / 100.0;
-			updatedSalary = (currentSalary) * (1 + percentIncrease);
-			//Lookup set fixed
-			cout << "Updated Salary: " << fixed << updatedSalary << endl << endl;
+		//Outputs updated salary with fixed precision
+		cout << "Updated Salary: " << fixed << updatedSalary << endl << endl;
 
-			project4Out.open("project4Out.txt", ios::app);
-			project4Out << "Last Name:" << LastName << endl;
-			project4Out << "First Name: " << FirstName << endl;
-			project4Out << "Current Salary: " << currentSalary << endl;
-			project4Out << "Percent Increase: " << percentIncrease << endl << endl;
+		//Outputs variables to output file using "Append" function of output methods
+		project4Out.open("project4Out.txt", ios::app);
+		project4Out << "Last Name:" << LastName << endl;
+		project4Out << "First Name: " << FirstName << endl;
+		project4Out << "Current Salary: " << currentSalary << endl;
+		project4Out << "Percent Increase: " << fixed << percentIncrease << endl << endl;
 
-			project4Out.close();
-
+		//Closes file
+		project4Out.close();
+		
 	}
-	/*while (!fin.eof())
-	{
-	sum += input;       // add to the sum
-	fin >> input;       // read next value
-	counter++;          // add to the count
-	}*/
-
-
-	/*cout << "Sum is " << setprecision(5) << sum << " and we found " << counter << " items." << endl;
-
-	double f = 3.14159;
-	cout << setprecision(3) << f << '\n';
-	cout << setprecision(5) << f << '\n';
-	cout << setprecision(9) << f << '\n';
-	cout << fixed;
-	cout << setprecision(5) << f << '\n';
-	cout << setprecision(9) << f << '\n';
-	*/
-
+	
 
 	system("pause");
 
 	return 0;
 }
-
